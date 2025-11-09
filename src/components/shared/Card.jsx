b3d8ba6f552130e8972f794023c8f2570064a1d8
lokman hakim <lokmanhakimshawon3@gmail.com>
2025-11-09 23:06:18 +0600

    update product page and layout

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -5,10 +5,10 @@ const Card = ({id,img,title,desc,price}) => {
     return (
         <div>
                 <div className='border border-slate-200 w-[165px] lg:w-[220px] cursor-pointer'>
-                    <img src={img} alt="TV" className='w-full h-[60%]'/>
+                    <img src={img} alt="TV" className='w-full max-h-[60%]'/>
                     <div className='p-1 lg:p-4'>
-                    <p className=" lg:font-bold text-sm lg:text-xl pt-2 ">{title}</p>
-                    <p className="font-semibold text-sm py-2">{desc.slice(0,30)}</p>
+                    <p className=" lg:font-bold text-sm lg:text-xl pt-2 ">{title.slice(0,30)}</p>
+                    <p className="font-semibold text-sm py-2">{desc.slice(0,35)}</p>
                     <p className="text-primary font-bold text-sm">{price} Tk</p>
                     </div>
                 </div>
